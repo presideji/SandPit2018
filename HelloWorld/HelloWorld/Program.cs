f4a65d20a6fa0ff85eebf24a3a6bdd8bba5286c6
Deji Akin <presideji@yahoo.com>
2018-12-25 15:49:16 +0000

    HELLO251218

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -240,6 +240,11 @@ namespace HelloWorld
             }
 
             */
+
+
+
+            /*
+             
             bool displaymenu = true;
             while (displaymenu == true)
             {
@@ -322,6 +327,55 @@ namespace HelloWorld
 
 
             Console.ReadLine();
+
+        }
+
+            */
+
+            {
+                //string myString = "My \"so-called\" life"; // the backsplash is used so that it can appear exactly as typed
+                //string myString = "What if I need a\nnew line?";
+                //string myString = "Go to your c:\\ drive";
+                //string myString = @"Go to your c:\ drive";
+                //string myString = String.Format("{1} = {0}", "First", "Second");
+                // string myString = string.Format("{0:C}", 123.45); // display currency
+                //string myString = string.Format("{0:N}", 1234544445);
+                //string myString = string.Format("Percentage: {0:P}", .123); // display percentage
+                // string myString = string.Format("Phone Number{0:(###) ###-####}", 1234567890);// way to format numbers
+                //string myString = " That summer we took threes across the board  ";
+                //myString = myString.Substring(6, 14);
+                // myString = myString.ToUpper(); // convert the text That summer we took threes across the board to upper case
+                // myString = myString.Replace(" ", "--");//put hyphen in the text "That summer we took threes across the board" 
+                // myString = myString.Remove(6, 14);
+
+                /*
+            //myString = String.Format("Lenght before: {0} -- Lenght after: {1}",
+            //    myString.Length, myString.Trim().Length);
+                */
+
+                /*
+
+                string myString = "";
+                for (int i = 0; i < 100; i++)
+                {
+                    myString += "--" + i.ToString();
+                }
+                 */
+
+                StringBuilder myString = new StringBuilder();
+                for (int i = 0; i < 100; i++)
+                {
+                    myString.Append("--");
+                    myString.Append(i);
+                }
+
+
+                Console.WriteLine(myString);
+                
+                Console.ReadLine();
+
+                  
+            }
 
         }
 
