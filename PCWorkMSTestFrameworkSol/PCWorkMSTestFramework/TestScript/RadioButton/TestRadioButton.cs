23e23057499fb182200582e7b1c852b167ce147e
Deji Akin <presideji@yahoo.com>
2019-01-10 05:52:34 +0000

    code10012019a

---
+++ PCWorkMSTestFrameworkSol/PCWorkMSTestFramework/TestScript/RadioButton/TestRadioButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using PCWorkMSTestFramework.ComponentHelper;
+using PCWorkMSTestFramework.Setting;
+
+namespace PCWorkMSTestFramework.TestScript.RadioButton
+{
+    [TestClass]
+    public class TestRadioButton
+    {
+        [TestMethod, TestCategory("SELECT A RADIO BUTTON")]
+        public void ChooseOneRadioButton()
+        {
+            NavigationHelper.NavigateToURl(ObjectRepository.Config.GetWebsite());
+
+            IWebElement radioElement = ObjectRepository.Driver.FindElement(By.Name("newsletter"));
+
+            radioElement.Click();
+        }
+    }
+}
